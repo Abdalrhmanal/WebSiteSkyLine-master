9a33c97a4ed87ff6b4c13775e6a444779060ac6a
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-03-03 22:02:38 +0300

    fix some error for build ture

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        "assets/js/vendor/jquery-1.12.4.min.js",
+        "assets/js/popper.min.js",
+        "assets/js/bootstrap.min.js",
+        "assets/js/main.js",
+        "assets/css/bootstrap.min.css",
+        "assets/css/owl.carousel.min.css",
+        "assets/css/slicknav.css",
+      ],
+    },
+  },
+});
