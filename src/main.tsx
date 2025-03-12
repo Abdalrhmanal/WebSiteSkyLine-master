126c080ffa50ea442cd765388f008efad61d567c
Ahmed Polat <ahmedpolat2002@gmail.com>
2025-03-12 15:24:14 +0300

    Update the site and optimization performance!

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home.tsx";
-import Contact from "./pages/Contact/Contact.tsx";
-import BusinessGallery from "./pages/BusinessGallery/BusinessGallery.tsx";
-import Preloader from "./components/Preloader.tsx";
-import "./index.css";
-import { ThemeContextProvider } from "./theme/ThemeContext";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeContextProvider>
-      <Preloader />
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="Business-Gallery" element={<BusinessGallery />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeContextProvider>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
