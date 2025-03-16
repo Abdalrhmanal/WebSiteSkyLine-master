f8cd6aa28d7d5ac1c582ec69c6ba23c696377234
Ahmed Polat <ahmedpolat2002@gmail.com>
2025-03-16 13:58:00 +0300

    merge

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,44 +1,12 @@
-import { useTranslation } from "react-i18next";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import AboutSection from "./AboutSection";
-import AskQuestionsSection from "./AskQuestionsSection";
-import DedicatedSupportSection from "./DedicatedSupportSection";
-
-import FeaturesSection from "./FeaturesSection";
 import OurValues from "./OurValues";
-import SliderArea from "./SliderArea";
-import TestimonialSection from "./TestimonialSection";
 import Vision from "./Vision";
 
 const Home = () => {
-  const { t } = useTranslation();
-  const menuItems = [
-    { name: "Home", path: "/" },
-    { name: "Business Gallery", path: "/business-gallery" },
-    { name: "Contact", path: "/contact" },
-  ];
-  const quickLinks = [
-    { name: t("footer.quickLinksList.0.name"), to: "#aboutUs" },
-    { name: t("footer.quickLinksList.1.name"), to: "/business-gallery" },
-    { name: t("footer.quickLinksList.2.name"), to: "#help" },
-    { name: t("footer.quickLinksList.3.name"), to: "#services" },
-    { name: t("footer.quickLinksList.4.name"), to: "/contact" },
-  ];
-
   return (
     <div style={{ overflow: "hidden" }}>
-      <Header menuItems={menuItems} />
-      <SliderArea />
       <Vision />
       <OurValues />
       {/* <DomainSearch /> */}
-      <FeaturesSection />
-      <AboutSection />
-      <DedicatedSupportSection />
-      <AskQuestionsSection />
-      <TestimonialSection />
-      <Footer quickLinks={quickLinks} />
     </div>
   );
 };
